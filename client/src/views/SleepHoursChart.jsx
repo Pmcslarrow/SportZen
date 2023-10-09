89f4f532b9a59042a00e968d80038897bf28b28b
pmcslarrow <pmcslarrow@icloud.com>
2023-10-09 12:26:56 -0700

    Visually more dynamic

--- client/src/views/SleepHoursChart.jsx
+++ client/src/views/SleepHoursChart.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 const SleepHoursChart = ({ sleepData }) => {
   const chartRef = useRef();
+  const [chartDimensions, setChartDimensions] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
     if (!sleepData) {
@@ -11,9 +12,21 @@ const SleepHoursChart = ({ sleepData }) => {
 
     const margin = { top: 20, right: 20, bottom: 50, left: 40 };
     const aspectRatio = 4 / 3; // Desired aspect ratio
-    const containerWidth = chartRef.current.clientWidth;
-    const width = containerWidth - margin.left - margin.right;
-    const height = width / aspectRatio - margin.top// Calculate height based on aspect ratio
+
+    const updateDimensions = () => {
+      const containerWidth = chartRef.current.clientWidth;
+      const maxWidth = 400; // Adjust the maximum width as needed
+      const width = Math.min(containerWidth, maxWidth) - margin.left - margin.right;
+      const height = width / aspectRatio - margin.top - margin.bottom;
+
+      setChartDimensions({ width, height });
+    };
+
+    // Initial dimensions setup
+    updateDimensions();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', updateDimensions);
 
     // Clear any existing content within the chartRef
     d3.select(chartRef.current).selectAll('*').remove();
@@ -21,22 +34,22 @@ const SleepHoursChart = ({ sleepData }) => {
     const svg = d3
       .select(chartRef.current)
       .append('svg')
-      .attr('width', containerWidth)
-      .attr('height', containerWidth / aspectRatio) // Set height based on aspect ratio
+      .attr('width', chartDimensions.width + margin.left + margin.right)
+      .attr('height', chartDimensions.height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
     const x = d3
       .scaleBand()
       .domain(Object.keys(sleepData))
-      .range([0, width])
+      .range([0, chartDimensions.width])
       .padding(0.1);
 
     const y = d3
       .scaleLinear()
       .domain([0, d3.max(Object.values(sleepData))])
       .nice()
-      .range([height, 0]);
+      .range([chartDimensions.height, 0]);
 
     svg
       .selectAll('.bar')
@@ -47,13 +60,13 @@ const SleepHoursChart = ({ sleepData }) => {
       .attr('x', (d) => x(d[0]))
       .attr('width', x.bandwidth())
       .attr('y', (d) => y(d[1]))
-      .attr('height', (d) => height - y(d[1]))
+      .attr('height', (d) => chartDimensions.height - y(d[1]))
       .style('fill', 'steelblue');
 
     svg
       .append('g')
       .attr('class', 'x-axis')
-      .attr('transform', `translate(0,${height})`)
+      .attr('transform', `translate(0,${chartDimensions.height})`)
       .call(d3.axisBottom(x))
       .selectAll('text')
       .style('text-anchor', 'end')
@@ -70,12 +83,13 @@ const SleepHoursChart = ({ sleepData }) => {
 
     return () => {
       // Cleanup when the component is unmounted
+      window.removeEventListener('resize', updateDimensions);
       svg.selectAll('*').remove();
     };
-  }, [sleepData]);
+  }, [sleepData, chartDimensions]);
 
   return (
-    <div style={{ width: '100%', paddingBottom: '75%' }} ref={chartRef}></div>
+    <div style={{ width: '100%', maxWidth: '400px' }} ref={chartRef}></div>
   );
 };
 
